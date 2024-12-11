c3439d32c0c1e75228b0d9f276406d822c358e4b
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-12-11 22:11:26 +0100

    recharts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,30 @@
-import { DashboardCard } from "@/components/dashboardCard";
-import { DollarSign } from "lucide-react";
 
+import { DashboardCard, DashboardCardContent } from "@/components/dashboard-card";
+import UserDataCard, { UserDataProps } from "@/components/user-data-card";
+import { db } from "@/lib/db";
+import { Calendar, CreditCard, DollarSign, PersonStanding, UserPlus, UserRoundCheck } from "lucide-react";
+import { eachMonthOfInterval, endOfMonth, format, formatDistanceToNow, startOfMonth } from "date-fns";
+import UserPurchaseCard, { UserPurchaseProps } from "@/components/user-purchase-card";
+
+
+
+
+
+  // Fetch Recent Users
+  const recentUsers = await db.user.findMany({
+    orderBy: {
+      createdAt: 'desc'
+    },
+    take: 7
+  });
+
+  // User Data
+ const UserData: UserDataProps[] = recentUsers.map((account) => ({
+    name: account.name || 'Unknown',
+    email: account.email || 'Unknown',
+    image: account.image || './mesh.png',
+    time: formatDistanceToNow(new Date(account.createdAt), {addSuffix: true})
+  }))
 
 export default function Home() {
   return (
@@ -34,7 +58,7 @@ export default function Home() {
                 description="All Time"
               />
             </section>
-             <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 transition-all">
+                 <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 transition-all">
           <DashboardCardContent>
             <section className="flex justify-between gap-2 pb-2">
               <p>Recent Users</p>
@@ -42,13 +66,16 @@ export default function Home() {
             </section>
             {UserData.map((data, index) => (
               <UserDataCard 
-                
-                name='nouha'
-                email='./photo.jpeg'
-                image='nouhailaechchnaoui@gmail.com'
-                time='2 days ago'
+                key={index}
+                name={data.name}
+                email={data.email}
+                image={data.image}
+                time={data.time}
               />
-          </div>
+            ))}
+          </DashboardCardContent>
+      
+
         </div>
       </div>
     </div>
