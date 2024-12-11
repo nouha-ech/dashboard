2565a9390bd78f88b010dd638f2f6e0bb9c2688b
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-12-11 21:08:58 +0100

    dashboard-card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import CustomBackground from "@/components/background";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <CustomBackground />
       </body>
     </html>
   );
---
+++ components/dashboardCard.tsx
@@ -0,0 +1,21 @@
+import {LucideIcon } from "lucide-react";
+interface DashboardCardProps {
+  label: string;
+  Icon: LucideIcon;
+  amount: any;
+  description: string;
+}
+export const DashboardCard = ({ label, Icon, amount, description }: DashboardCardProps) => {
+    return (
+      <div className="bg-slate-100/40 border  shadow flex w-full flex-col gap-3 rounded-[6px] p-5">
+        <section className="flex justify-between gap-2">
+          <p className="text-sm">{label}</p>
+          <Icon className="h-4 w-4" />
+        </section>
+        <section className="flex flex-col gap-2">
+          <h2 className="font-semibold text-2xl">{amount}</h2>
+          <p className="text-sm">{description}</p>
+        </section>
+      </div>
+    );
+}
